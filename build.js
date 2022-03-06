c48537407bf2e47db24d0266d55445b97e59e8a4
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2022-03-06 12:23:16 +0300

    Added new font awesome

--- build.js
+++ build.js
@@ -1,27 +1,35 @@
 import {readdir, readFile, writeFile} from 'fs/promises'
 import {minify} from "uglify-js";
+import cpy from "cpy";
 
-const files = {};
-for (const file of [
-    'node_modules/vue/dist/vue.global.js',
-    'js/component.js',
-]) {
-    files[file] = await readFile(file, 'utf-8');
-}
+(async function js() {
+    const files = {};
+    for (const file of [
+        'node_modules/vue/dist/vue.global.js',
+        'js/component.js',
+    ]) {
+        files[file] = await readFile(file, 'utf-8');
+    }
 
-const components = (await readdir('components')).map(it => it.replace(/\.\w*$/, ''));
+    const components = (await readdir('components')).map(it => it.replace(/\.\w*$/, ''));
 
-files['main.js'] = /*language=JavaScript*/ `
-    window.addEventListener('load', async () => {
-        const app = Vue.createApp({});
-        await Promise.all(${JSON.stringify(components)}
-            .map(async it => app.component('v:' + it, await component('./components/' + it + '.vue'))));
-        app.mount(document.body);
-    });
-`;
+    files['main.js'] = /*language=JavaScript*/ `
+        window.addEventListener('load', async () => {
+            const app = Vue.createApp({});
+            await Promise.all(${JSON.stringify(components)}
+                .map(async it => app.component('v:' + it, await component('./components/' + it + '.vue'))));
+            app.mount(document.body);
+        });
+    `;
 
-const bundle = minify(files);
-await writeFile('index.js', bundle.code, 'utf-8');
+    const bundle = minify(files);
+    await writeFile('index.js', bundle.code, 'utf-8');
+})();
+
+(async function copy() {
+    cpy('node_modules/font-awesome/css/*', 'css', {flat: true});
+    cpy('node_modules/font-awesome/fonts/*', 'fonts', {flat: true});
+})();
 
 
 
