ad04d683f2afe062bb7c9ccad7ed1438d5b8eb91
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2022-03-07 02:51:51 +0300

    VK members section

--- components/main.vue
+++ components/main.vue
@@ -2,6 +2,7 @@
   <main xmlns:v>
     <v:section-top-hero></v:section-top-hero>
     <v:section-info></v:section-info>
+    <v:section-vk-members></v:section-vk-members>
     <v:section-coach></v:section-coach>
   </main>
 </template>
---
+++ components/section-vk-members.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="vk-members">
+    <div class="container">
+      <div class="row">
+        <div class="col6">
+          <h2>Присоединяйтесь<br>к нам в <i class="fa fa-vk"></i>!</h2>
+        </div>
+        <div class="col6">
+          <div id="vk-members" class="widget-vk"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  mounted() {
+    VK.Widgets.Group("vk-members", {mode: 3, width: "auto", color1: 'F1C909', color2: '201C20', color3: '725A1C'}, 133258069);
+  }
+}
+</script>
+
