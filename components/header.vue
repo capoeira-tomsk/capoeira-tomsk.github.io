4a6b23fc42c9ea79518a57ce221b7bb7ca6f0737
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2022-03-06 14:22:11 +0300

    New style

--- components/header.vue
+++ components/header.vue
@@ -1,26 +1,26 @@
 <template>
   <header>
-    <nav>
-      <div class="container">
+    <div class="container">
+      <nav>
         <div class="navbar-header">
           <a href="#" class="navbar-brand"><img class="brand-icon" src="img/brand-icon.png"/></a>
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-            <i class="fa fa-bars"></i>
-          </button>
+          <!--          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">-->
+          <!--            <i class="fa fa-bars"></i>-->
+          <!--          </button>-->
         </div>
-          <ul class="collapse" id="#navbar">
-            <li><a href="#"><i class="fa fa-home"></i> Главная</a></li>
-            <li><a href="#trainer"><i class="fa fa-user"></i> О тренере</a></li>
-            <li><a href="#contacts"><i class="fa fa-calendar"></i> Расписание</a></li>
-            <li><a href="#news"><i class="fa fa-vk"></i> Новости</a></li>
-            <li><a href="articles.html"><i class="fa fa-book"></i> Статьи</a></li>
-          </ul>
-          <ul class="nav navbar-nav">
-            <li><a href="https://vk.com/capoeiratomsk" class="btn btn-round"><i class="fa fa-vk"></i></a></li>
-            <li><a href="https://www.instagram.com/capoeiratomsk/" class="btn btn-round"><i class="fa fa-instagram"></i></a></li>
-            <li><a href="tel:+7-903-950-34-16" class="btn btn-round"><i class="fa fa-phone"></i></a></li>
-          </ul>
-      </div>
-    </nav>
+        <ul class="navbar-collapse" id="#navbar">
+          <li><a href="#">Главная</a></li>
+          <li><a href="#trainer">О тренере</a></li>
+          <li><a href="#contacts">Расписание</a></li>
+          <li><a href="#news">Новости</a></li>
+          <li><a href="articles.html">Статьи</a></li>
+        </ul>
+        <ul class="navbar-right">
+          <li><a href="https://vk.com/capoeiratomsk" class="btn btn-round"><i class="fa fa-vk"></i></a></li>
+          <li><a href="https://www.instagram.com/capoeiratomsk/" class="btn btn-round"><i class="fa fa-instagram"></i></a></li>
+          <li><a href="tel:+7-903-950-34-16" class="btn btn-round"><i class="fa fa-phone"></i></a></li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
