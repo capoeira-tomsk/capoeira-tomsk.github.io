ab7c8e344db4b897fddc58850f3522a513cc542b
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2022-03-07 15:18:28 +0300

    Footer section

--- components/footer.vue
+++ components/footer.vue
@@ -1,3 +1,13 @@
 <template>
-  <footer>Footer</footer>
+  <footer>
+    <div class="container">
+      <p>Капоэйра в Томске</p>
+      <ul class="social-links">
+        <li><a href="https://vk.com/capoeiratomsk"><i class="fa fa-vk"></i></a></li>
+        <li><a href="https://www.instagram.com/capoeiratomsk/"><i class="fa fa-instagram"></i></a></li>
+        <li><a href="tel:+7-903-955-63-53"><i class="fa fa-phone"></i></a></li>
+      </ul>
+      <p>{{ new Date().getFullYear() }}</p>
+    </div>
+  </footer>
 </template>
