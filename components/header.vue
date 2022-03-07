0b4ca0a361302255fec7cbaf7b2bc52fd5c8c7ff
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2022-03-08 02:42:09 +0300

    Separate component for social links

--- components/header.vue
+++ components/header.vue
@@ -15,11 +15,7 @@
           <li><a href="#news">Новости</a></li>
           <li><a href="articles.html">Статьи</a></li>
         </ul>
-        <ul class="navbar-right social-links">
-          <li><a href="https://vk.com/capoeiratomsk"><i class="fa fa-vk"></i></a></li>
-          <li><a href="https://www.instagram.com/capoeiratomsk/"><i class="fa fa-instagram"></i></a></li>
-          <li><a href="tel:+7-903-955-63-53"><i class="fa fa-phone"></i></a></li>
-        </ul>
+        <v:social-links xmlns:v class="navbar-right"></v:social-links>
       </nav>
     </div>
   </header>
