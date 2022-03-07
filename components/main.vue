5abfde9cf2ef0e15d91075aa7901c781bc006378
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2022-03-07 14:55:26 +0300

    Section news

--- components/main.vue
+++ components/main.vue
@@ -5,5 +5,6 @@
     <v:section-vk-members></v:section-vk-members>
     <v:section-coach></v:section-coach>
     <v:section-schedule></v:section-schedule>
+    <v:section-news></v:section-news>
   </main>
 </template>
---
+++ components/section-news.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="news" id="news">
+    <div class="container">
+      <div class="row">
+        <div class="col4"></div>
+        <div class="col8">
+          <div id="vk-news" class="widget-vk"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  mounted() {
+    VK.Widgets.Group("vk-news", {mode: 4, wide: 1, width: "auto", height: window.innerHeight, color1: 'FFFFFF', color2: '201C20', color3: '725A1C'}, 133258069);
+  }
+}
+</script>
