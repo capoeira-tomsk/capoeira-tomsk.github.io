37d1706a6062732172a600db9b47812fb954ad88
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2022-03-23 10:45:26 +0300

    Removed link to instagram

--- components/social-links.vue
+++ components/social-links.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="social-links">
     <li><a href="https://vk.com/capoeiratomsk"><i class="fa fa-vk"></i></a></li>
-    <li><a href="https://www.instagram.com/capoeiratomsk/"><i class="fa fa-instagram"></i></a></li>
+<!--    <li><a href="https://www.instagram.com/capoeiratomsk/"><i class="fa fa-instagram"></i></a></li>-->
     <li><a href="tel:+7-903-955-63-53"><i class="fa fa-phone"></i></a></li>
   </ul>
 </template>
